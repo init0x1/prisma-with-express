cc4ab1760c952a7e77652ad23a6e9d940eadd1f4
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-03-17 21:40:33 +0200

    adding vakidate user login

--- middlewares/validatior.js
+++ middlewares/validatior.js
@@ -36,4 +36,33 @@ const validateNewUserData = (req, res, next) => {
     }
 };
 
-module.exports = { validateNewUserData };
+// validate user login data
+const userLoginSchema = joi.object({
+    email: joi.string().email().trim().required()
+        .messages({
+            'string.email': 'Please provide a valid email address',
+            'any.required': 'Email is required'
+        }),
+    password: joi.string().min(8).required()
+        .messages({
+            'string.min': 'Password must be at least 8 characters',
+            'any.required': 'Password is required'
+        })
+});
+
+const validateUserLoginData = (req, res, next) => {
+    try {
+        const { error } = userLoginSchema.validate(req.body, { abortEarly: false });
+
+        if (error) {
+            const errorMessages = error.details.map((detail) => detail.message);
+            throw new AppError('Invalid input data', 400, errorMessages);
+        }
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports = { validateNewUserData, validateUserLoginData };
--- routes/user.router.js
+++ routes/user.router.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const {validateNewUserData} = require('../middlewares/validatior');
+const {validateNewUserData,validateUserLoginData} = require('../middlewares/validatior');
 const {createUser,userLogin} = require('../controllers/user.controller');
 
 
 const userRouter = express.Router();
 
 userRouter.post('/',validateNewUserData,createUser);
-userRouter.post('/login',userLogin);
+userRouter.post('/login',validateUserLoginData,userLogin);
 
 module.exports = userRouter;
