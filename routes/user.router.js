ab25ffe912af525728a64c8090e237f37a72344c
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-03-18 00:45:14 +0200

    update user routes

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {validateNewUserData,validateUserLoginData} = require('../middlewares/validatior');
+const {validateNewUserData,validateUserLoginData} = require('../middlewares/validator/user.validator');
 const {createUser,userLogin} = require('../controllers/user.controller');
 
 
